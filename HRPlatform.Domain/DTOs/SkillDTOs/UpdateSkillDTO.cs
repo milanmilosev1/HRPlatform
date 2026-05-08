ba74e30b6adac2ab394013cf042bd65a1ad36ee5
milanmilosev1 <milosevmil@gmail.com>
2026-05-08 15:25:00 +0200

    implemented skill service

---
+++ HRPlatform.Domain/DTOs/SkillDTOs/UpdateSkillDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HRPlatform.Domain.DTOs.SkillDTOs
+{
+    public class UpdateSkillDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<Guid> CandidateIds { get; set; } = [];
+    }
+}
--- HRPlatform.Domain/Services/ISkillServices.cs
+++ HRPlatform.Domain/Services/ISkillServices.cs
@@ -1,13 +1,14 @@
-﻿using HRPlatform.Domain.Models;
+﻿using HRPlatform.Domain.DTOs.SkillDTOs;
+using HRPlatform.Domain.Models;
 
 namespace HRPlatform.Domain.Services
 {
     public interface ISkillServices
     {
-        Task<Skill> AddSkillAsync(Skill skill);
+        Task<Skill> AddSkillAsync(CreateSkillDTO skill);
         Task RemoveSkillAsync(Guid id);
-        Task<Skill> UpdateSkillInfoAsync(Skill newSkill);
-        Task<Skill> GetSkillByIdAsync(Guid skillId);
+        Task<Skill?> UpdateSkillInfoAsync(UpdateSkillDTO newSkill);
+        Task<Skill?> GetSkillByIdAsync(Guid skillId);
         Task<List<Skill>> GetSkillsAsync();
     }
 }
